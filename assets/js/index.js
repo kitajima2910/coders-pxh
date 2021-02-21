dd03389397a083431c96858d4c5926dcd7382d77
Kitajima2910 <kitajima2910@gmail.com>
2021-02-21 22:17:50 +0700

    hoaipx/20210221

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,14 +8,4 @@ $(() => {
 		$(tabpanelShow).attr("aria-hidden", "false");
 		$(tabpanelShow).siblings().attr("aria-hidden", "true");
 	});
-
-	// Load HTML: JavaScript
-	let pathname = window.location.pathname;
-
-	// Fix on Github
-	$('#html_javascript').load(pathname === '/coders-pxh/' ? 'includes/javascript/index.html' : './../../includes/javascript/index.html');
-
-	setTimeout(() => {
-		sh_highlightDocument();
-	}, 750);
 });
