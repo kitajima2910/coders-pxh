dfe6d9947b3310a701e4d65e370e1d15b2f7045f
Kitajima2910 <kitajima2910@gmail.com>
2021-02-21 22:31:22 +0700

    hoaipx/20210221

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,3 @@
 $(() => {
-	//Basic function
-	$("[role='tab']").click(function (e) {
-		e.preventDefault();
-		$(this).attr("aria-selected", "true");
-		$(this).parent().siblings().children().attr("aria-selected", "false");
-		var tabpanelShow = $(this).attr("href");
-		$(tabpanelShow).attr("aria-hidden", "false");
-		$(tabpanelShow).siblings().attr("aria-hidden", "true");
-	});
+	
 });
